c8cc358b42682ecb0caa39d328400591e5e591fb
rodrigues.tallyto <rodrigues.tallyto@hotmail.com>
2020-03-15 18:59:27 -0400

    Criando a pagina de projetos

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,8 @@ routes.get("/", (req, res)=> {
   res.sendFile(path.join(__dirname,'public/index.html'))
 })
 
+routes.get("/projetos", (req, res)=> {
+  res.sendFile(path.join(__dirname,'public/projetos.html'))
+})
+
 module.exports = routes
